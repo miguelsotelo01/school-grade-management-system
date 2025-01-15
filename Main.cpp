b32482dd1390fb5242350fc1b10fdbf2cdf62121
Miguel Angel Sotelo <miky.a.sotelo.97@gmail.com>
2025-01-14 23:45:43 -0300

    estructura de estudiante agregada, correcciones en el main y readme

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+
+struct estudiante{
+    char nombre[50];
+    char apellido[50];
+    float DNI;
+    char curso[50];
+    char division[50];
+    char inasistenciasPorTrimestre[50];
+    bool estadoFinal;
+}
+int main(){
+    
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # school-grade-management-system
 TFI de la materia Algoritmos y estructuras de datos
 Universidad Tecnologica Nacional Facultad Regional Tucuman
+Comisión: 1k11 anexo concepción. 
+Título del Proyecto: Sistema de Gestión de Notas. 
